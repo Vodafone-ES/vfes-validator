0ba414eaffcbc77d09cd1641aaffe68c221e28f0
rerades <rerades@siete3.com>
2019-08-28 15:03:54 +0200

    TDD example

---
+++ src/rules/cifAlt.js
@@ -0,0 +1,15 @@
+/**
+ * Validación de CIF
+ */
+/*
+// str -> str
+const toUpperCase = cif => cif.toUpperCase();
+
+// str -> boolean
+const validFormat = cif => !/^[A-Z][0-9]{7}[A-Z]$/g.test(cif);
+
+const letters = ['J', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+const digits = cif => cif.substr(1, cif.length - 2);
+const letter = cif => cif.substr(0, 1);
+const control = cif => cif.substr(cif.length - 1);
+*/
