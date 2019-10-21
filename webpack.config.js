3e035ea47ed1db26e6b8fd948399ab8854c96eaf
Edu Iglesias <eduiglesias@MacBook-Pro-de-Edu.local>
2019-10-21 18:55:40 +0200

    Include property entry

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: process.env.NODE_ENV || 'development',
+    entry: './src/validator.js',
     output: {
         path: path.resolve('./dist'),
         filename: 'bundle.js',
